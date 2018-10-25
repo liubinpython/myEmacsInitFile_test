6e792ba3e1581525adba2ccf67691cfb0a065c41
albe <liubinzy@gmail.com>
2018-10-25 13:45:04 +0800

    supports mit-scheme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # myEmacsInitFile_test
-my emacs init file for studying emacs purpose
+
+## History of Updates
+- 2017 My emacs init file for studying emacs purpose
+- 2018-10-25 Supports integrated mit-scheme for learning SICP!
+
--- init.el
+++ init.el
@@ -26,6 +26,7 @@
 (popwin-mode t)
 
 ;;--------END Install Package--------
+
 ;;Zilongshanren tutorial 
 (tool-bar-mode -1) ;;turn off the tool bar in frame
 (scroll-bar-mode -1) ;;turn off the scroll bar in frame
@@ -39,23 +40,39 @@
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 
+;;-------- Albert Personal SETTING --------
 ;;--------ORG-MODE SETTING--------
 (setq org-agenda-files '("~\org"))
 (global-set-key (kbd "C-c a") 'org-agenda)
-
 ;;Let emacs-lisp has grammer highlight in org-mode
 (require 'org)
 (setq org-src-fontify-natively t)
-;;2018-10-24 Albert added to support scheme
-(setq scheme-program-name "mit-scheme")
 
+;;2018-10-24 Albert added to support scheme
+(setenv "MITSCHEME_LIBRARY_PATH"
+        "C:\\Program Files (x86)\\MIT-GNU Scheme\\lib")
+;;This is the binary name of used scheme interpretor.
+(setq scheme-program-name "mit-scheme")
 ;;always do syntax highlighting, also highlight parens
 (global-font-lock-mode 1)
 (setq show-paren-delay 0
       show-paren-style 'parenthesis)
 (show-paren-mode 1)
-;;end
-;;--------END SETTING--------
+;;Define F5 to run-scheme
+(add-hook 'scheme-mode-hook
+	  (lambda ()(local-set-key (kbd "<f5>") 'run-scheme-in-emacs)))
+(defun run-scheme-in-emacs ()
+  (interactive)
+  (let ((file-name (buffer-file-name)))
+    (switch-to-buffer-other-window "*scheme*")
+    (erase-buffer)
+    (run-scheme scheme-program-name)
+;;    (switch-to-buffer (get-buffer "*scheme*"))
+    (scheme-load-file file-name)
+    (other-window 1))
+  )
+;;2018-10-24 end
+
 ;;enable recent mode
 (require 'recentf)
 (recentf-mode 1)
@@ -73,6 +90,8 @@
 (global-set-key (kbd "<f2>") 'open-my-init-file)
 ;;END
 
+;;--------END Albert Personal SETTING--------
+
 ;;This field used to enable package which has been installed
 (global-company-mode t)
 (global-linum-mode t);; enabled the linum mode
@@ -86,7 +105,7 @@
  '(org-agenda-files nil)
  '(package-selected-packages
    (quote
-    (popwin zenburn-theme anti-zenburn-theme monokai-theme ahungry-theme company))))
+    (racket-mode geiser popwin zenburn-theme anti-zenburn-theme monokai-theme ahungry-theme company))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
